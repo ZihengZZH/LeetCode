706a0ac54824b7314bd4563efe19aa67a9460613
Ziheng ZHANG <zihengzhang1025@gmail.com>
2017-11-21 13:40:39 +0000

    Add files via upload

---
+++ WildcardMatching.py
@@ -0,0 +1,40 @@
+'''
+
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+
+'''
+
+
+def isMatch(s,p):
+    length = len(s)
+    if len(p) - p.count('*') > length:
+        return False
+    dp = [True] + [False]*length
+    for i in p:
+        if i != '*':
+            for n in reversed(range(length)):
+                dp[n+1] = dp[n] and (i == s[n] or i == '?')
+        else:
+            for n in range(1, length+1):
+                dp[n] = dp[n-1] or dp[n]
+        dp[0] = dp[0] and i == '*'
+    return dp[-1]
+
+if __name__ == "__main__":
+    s = "aad"
+    p = "c*a*d"
+    print(s,p,isMatch(s,p))
